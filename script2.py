e837f964a69a2b17b3dbe304fb7989e55e942062
JainaQ <dhirenjay.morjaria@flemingcollege.ca>
2023-01-18 13:41:48 -0500

    Modifed both files in the repo clone
    - Delated a print() statement from script.py
    - added the deleted print() statement from script1.py to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #this also prints lines from the zen of python
 print("Complex is better than complicated.")
 print("Special cases aren't special enough to break the rules.")
+print("Readability counts.")
